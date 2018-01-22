5a6747e4608ee7b5195b14b96ce463c079e8e8e2
ali404 <acerrahoglu.ali@gmail.com>
2018-01-22 22:49:29 +0000

    added documentation for the functions created

--- robot1/motors.py
+++ robot1/motors.py
@@ -10,10 +10,17 @@ rotateMotor = None
 motorsInit = False
 
 def checkInit():
+    """Checks if motors are initialised and init them if not."""
     if not motorsInit:
         initMotors()
 
 def initMotors():
+    """Initialised motors for the robot arm v1.
+
+    Throws assertion error if one of the sensors / arm
+    is not connected.
+    """
+
     global touchSensor
     global colorSensor
     global grabMotor
@@ -39,6 +46,14 @@ def initMotors():
     motorsInit = True
 
 def grab(pos=-50, speed=300, stop='hold'):
+    """ Rotates the grab motor to grab an item
+
+    Arguments:
+    pos -- absolute position of the motor (def: -50 deg)
+    speed -- speed in tachions (def: 300)
+    stop -- stop action to be performed after motor stops (def: 'hold')
+    """
+
     checkInit()
     grabMotor.run_to_abs_pos(
         position_sp= -50,
@@ -48,6 +63,14 @@ def grab(pos=-50, speed=300, stop='hold'):
     utils.waitFor(grabMotor)
 
 def release(pos=20, speed=300, stop='hold'):
+    """ Rotates the grab motor to release an item
+
+    Arguments:
+    pos -- absolute position of the motor (def: 20 deg)
+    speed -- speed in tachions (def: 300)
+    stop -- stop action to be performed after motor stops (def: 'hold')
+    """
+
     checkInit()
     grabMotor.run_to_abs_pos(
         position_sp=pos,
@@ -57,6 +80,15 @@ def release(pos=20, speed=300, stop='hold'):
     utils.waitFor(grabMotor)
 
 def rotate(pos=0, speed=300, stop='brake'):
+    """ Rotates the arm (doesn't depend on the angle of the arm) (Horizontally).
+    Could result in the arm hitting an object.
+
+    Arguments:
+    pos -- absolute position of the motor angle (def: 0 deg)
+    speed -- speed in tachions (def: 300)
+    stop -- stop action (performed after the motor stops) (def: 'brake')
+    """
+
     checkInit()
     rotateMotor.run_to_abs_pos(
         position_sp=pos,
@@ -66,12 +98,26 @@ def rotate(pos=0, speed=300, stop='brake'):
     utils.waitFor(rotateMotor)
 
 def rotateInitialPos():
+    """Rotates the arm to the initial position: 45 deg"""
+
     rotate(pos=45, speed=300, stop='hold')
 
 def rotateFinalPos():
+    """Rotate the arm to the final position: -420 deg"""
+
     rotate(pos=-420, stop='hold')
 
 def moveAngle(pos=0, speed=300, stop='brake'):
+    """Moves the angle of the arm (Vertically)
+
+    Arguments:
+    pos -- absolute position of the motor angle (def: 0 deg)
+    speed -- speed in tachions (def: 300)
+    stop -- stop action (performed after the motor stops) (def: 'brake')
+    Change stop action if you want the angle to remain in place after
+    the motor stops executing (stop = 'hold' does that!)
+    """
+
     checkInit()
     angleMotor.run_to_abs_pos(
         position_sp=pos,
@@ -81,7 +127,13 @@ def moveAngle(pos=0, speed=300, stop='brake'):
     utils.waitFor(angleMotor)
 
 def moveUpwards():
+    """Moves the arm up vertically"""
     moveAngle(pos=-90, stop='hold')
 
 def moveDownwards():
+    """Moves the arm down vertically.
+
+    Used before grabbing or releasing an item
+    """
+
     moveAngle(pos=180, stop='hold')
--- robot1/utils.py
+++ robot1/utils.py
@@ -6,6 +6,11 @@ import time
 # asserts all sensors in sensors array are connected
 #
 def assertAll(*sensors):
+    """ Asserts all sensors / motors are connected.
+
+    Arguments:
+    *sensors -- variable list of arguments of sensors / motors
+    """
     for sensor in sensors:
         assert sensor.connected == True, \
         "{0} at {1} is not connected".format(
@@ -16,6 +21,15 @@ def assertAll(*sensors):
 
 
 def waitFor(sensor, waitForNext=True):
+    """ Waits for motor / sensor to stop running.
+    If waitForNext is False, after the motor stops, it doesn't wait for 0.5 sec
+
+    Arguments:
+    sensor -- specifies for what sensor / motor to be waiting for 
+    waitForNext -- specifies if function should wait 0.5 sec after
+    the motor stops. (def True)
+    """
+
     time.sleep(0.1)
     while "running" in sensor.state:
         time.sleep(0.1)
