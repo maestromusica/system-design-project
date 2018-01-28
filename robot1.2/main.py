cb7cf3fcf509f1e4903cfef587f23ff5557116c9
ali404 <acerrahoglu.ali@gmail.com>
2018-01-28 19:43:15 +0000

    deleted unneccessary files

--- robot1.2/main.py
+++
@@ -1,7 +0,0 @@
-#! /usr/env/bin python3
-from framework.motors import LargeMotor, MediumMotor
-from framwrok.sync import Synchroniser
-
-sync = Synchroniser(fileName="version_0.1")
-grabMotor = LargeMotor('outA', sync)
-angleMotor = MediumMotor('outB', sync)
