9a64305af248cb1f25bb65f6c30cbb9d06bb0640
ali404 <acerrahoglu.ali@gmail.com>
2018-03-13 16:32:06 +0000

    added on keypress enter to send the command

--- src/client-app/app/components/Development/AxisSection.js
+++ src/client-app/app/components/Development/AxisSection.js
@@ -13,7 +13,43 @@ export default class AxisSection extends Component {
     moveX: undefined,
     moveY: undefined,
     moveZ: undefined,
-    moveGrabber: undefined
+    moveGrabber: undefined,
+    moveRotate: undefined
+  }
+
+  _sendMoveX = () => {
+    this.props.actions.moveX(this.state.moveX)
+    this.setState({
+      moveX: undefined
+    })
+  }
+
+  _sendMoveY = () => {
+    this.props.actions.moveY(this.state.moveY)
+    this.setState({
+      moveY: undefined
+    })
+  }
+
+  _sendMoveZ = () => {
+    this.props.actions.moveZ(this.state.moveZ)
+    this.setState({
+      moveZ: undefined
+    })
+  }
+
+  _sendMoveGrabber = () => {
+    this.props.actions.grab(this.state.moveGrabber)
+    this.setState({
+      moveGrabber: undefined
+    })
+  }
+
+  _sendMoveRotate = () => {
+    this.props.actions.rotate(this.state.moveRotate)
+    this.setState({
+      moveRotate: undefined
+    })
   }
 
   render() {
@@ -32,13 +68,15 @@ export default class AxisSection extends Component {
             this.setState({
               moveX: ev.target.value
             })
-          }} placeholder="moveX" value={this.state.moveX} />
-          <SectionOptionButton onClick={ev => {
-            this.props.actions.moveX(this.state.moveX)
-            this.setState({
-              moveX: undefined
-            })
-          }} disabled={disabledX}>
+          }} placeholder="moveX" value={this.state.moveX} onKeyPress={ev => {
+            if(ev.key == 'Enter') {
+              this._sendMoveX()
+            }
+          }} />
+          <SectionOptionButton
+            onClick={ev => this._sendMoveX()}
+            disabled={disabledX}
+          >
             Send Command
           </SectionOptionButton>
         </SectionOption>
@@ -48,61 +86,80 @@ export default class AxisSection extends Component {
             this.setState({
               moveY: ev.target.value
             })
-          }} placeholder="moveY" value={this.state.moveY} />
-          <SectionOptionButton onClick={ev => {
-            this.props.actions.moveY(this.state.moveY)
-            this.setState({
-              moveY: undefined
-            })
-          }} disabled={disabledY}>
+          }} placeholder="moveY" value={this.state.moveY} onKeyPress={ev => {
+            if(ev.key == 'Enter') {
+              this._sendMoveY()
+            }
+          }}/>
+          <SectionOptionButton onClick={ev => this._setMoveY()} disabled={disabledY}>
             Send Command
           </SectionOptionButton>
         </SectionOption>
         <SectionOption>
           <SectionOptionTitle>MoveZ</SectionOptionTitle>
-          <SectionOptionInput onChange={ev => {
-            this.setState({
-              moveZ: ev.target.value
-            })
-          }} placeholder="moveZ" value={this.state.moveZ} />
-          <SectionOptionButton onClick={ev => {
-            this.props.actions.moveZ(this.state.moveZ)
-            this.setState({
-              moveZ: undefined
-            })
-          }} disabled={disabledZ}>
+          <SectionOptionInput
+            onChange={ev => {
+              this.setState({
+                moveZ: ev.target.value
+              })
+            }}
+            placeholder="moveZ"
+            value={this.state.moveZ}
+            onKeyPress={ev => {
+              if(ev.key == 'Enter') {
+                this._sendMoveZ()
+              }
+            }}
+          />
+          <SectionOptionButton
+            onClick={ev => this._sendMoveZ()}
+            disabled={disabledZ}>
             Send Command
           </SectionOptionButton>
         </SectionOption>
         <SectionOption>
           <SectionOptionTitle>Move grabber</SectionOptionTitle>
-          <SectionOptionInput onChange={ev => {
-            this.setState({
-              moveGrabber: ev.target.value
-            })
-          }} placeholder="move grabber" value={this.state.moveGrabber} />
-          <SectionOptionButton onClick={ev => {
-            this.props.actions.grab(this.state.moveGrabber)
-            this.setState({
-              moveGrabber: undefined
-            })
-          }} disabled={disabledGrabber}>
+          <SectionOptionInput
+            onChange={ev => {
+              this.setState({
+                moveGrabber: ev.target.value
+              })
+            }}
+            placeholder="move grabber"
+            value={this.state.moveGrabber}
+            onKeyPress={ev => {
+              if(ev.key == 'Enter') {
+                this._sendMoveGrabber()
+              }
+            }}
+          />
+          <SectionOptionButton
+            onClick={ev => this._sendMoveGrabber()}
+            disabled={disabledGrabber}
+          >
             Send Command
           </SectionOptionButton>
         </SectionOption>
         <SectionOption>
           <SectionOptionTitle>Rotate grabber</SectionOptionTitle>
-          <SectionOptionInput onChange={ev => {
-            this.setState({
-              moveRotate: ev.target.value
-            })
-          }} placeholder="rotate grabber" value={this.state.moveRotate} />
-          <SectionOptionButton onClick={ev => {
-            this.props.actions.rotate(this.state.moveRotate)
-            this.setState({
-              moveRotate: undefined
-            })
-          }} disabled={disabledRotate}>
+          <SectionOptionInput
+            onChange={ev => {
+              this.setState({
+                moveRotate: ev.target.value
+              })
+            }}
+            placeholder="rotate grabber"
+            value={this.state.moveRotate}
+            onKeyPress={ev => {
+              if(ev.key == 'Enter') {
+                this._sendMoveRotate()
+              }
+            }}
+          />
+          <SectionOptionButton 
+            onClick={ev => this._sendMoveRotate()}
+            disabled={disabledRotate}
+          >
             Send command
           </SectionOptionButton>
         </SectionOption>
